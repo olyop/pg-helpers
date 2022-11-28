46afcaf5267e0e69ada6238369c30293255b3c65
Oliver Plummer <oliver.plummer@outlook.com>
2022-11-28 15:22:06 +1100

    Update

--- src/add-prefix.ts
+++ src/add-prefix.ts
@@ -1,2 +1,2 @@
 export const addPrefix = (values: readonly string[], prefix?: string) =>
-	prefix ? values.map(value => `${prefix}.${value}`) : values;
+	prefix ? values.map(value => `${prefix}.${value}`) : (values as string[]);
